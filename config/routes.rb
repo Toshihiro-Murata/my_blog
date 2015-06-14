87f05ac9485cc36e5c6531409dd2ee86285760e4
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-14 11:18:52 +0900

    Add new,create,index for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/posts/new' => 'posts#new'
+  get '/posts/:id' => 'posts#show'
+  post '/posts/create' => 'posts#create'
+  get '/posts' => 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
