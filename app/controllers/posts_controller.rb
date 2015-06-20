fcd467c4f840d48c63d2172051e0aaeb3b685520
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-20 19:19:44 +0900

    Add paginate to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.order('created_at DESC').all
+    #@posts = Post.order('created_at DESC').all
+    @posts = Post.page(params[:page])
   end
 
   def new
